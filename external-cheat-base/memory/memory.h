0218146f16c8786a7db7b6e582702667db51f217
Guyy99 <115192045+Guyy99@users.noreply.github.com>
2024-01-14 14:58:21 +0000

    Update memory.h

--- external-cheat-base/memory/memory.h
+++ external-cheat-base/memory/memory.h
@@ -13,8 +13,16 @@ namespace memory
 	uintptr_t GetProcID(const wchar_t* process);
 	uintptr_t GetModuleBaseAddress(uintptr_t procID, const wchar_t* module);
 
-	template <typename T> T Read(uintptr_t address);
-	template <typename T> bool Write(uintptr_t address, T value);
+	template <typename T> T Read(uintptr_t address)
+	{
+		T ret;
+		ReadProcessMemory(gHandle, address, &ret, sizeof(T), nullptr);
+		return ret;
+	}
+	template <typename T> bool Write(uintptr_t address, T value)
+	{
+		return WriteProcessMemory(gHandle, address, &value, sizeof(T), nullptr);
+	}
 
 
 }
