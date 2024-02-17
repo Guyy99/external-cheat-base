cc84da3e3e15a47604128307af8554d4ddd58736
Guyy99 <115192045+Guyy99@users.noreply.github.com>
2024-02-17 17:11:20 +0000

    Update vector.h

--- external-cheat-base/math/vector.h
+++ external-cheat-base/math/vector.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cmath>
+#include <numbers>
+
 struct vec4
 {
 	float w, x, y, z;
@@ -15,6 +18,15 @@ struct vec3
 	vec3 operator-(vec3 other)
 	{
 		return { this->x - other.x, this->y - other.y, this->z - other.z };
+	}
+
+	vec3 RelativeAngle()
+	{
+		return {
+			std::atan2(-z, std::hypot(x, y)) * (180.0f / std::numbers::pi_v<float>),
+			std::atan2(y, x) * (180.0f / std::numbers::pi_v<float>),
+			0.0f
+		};
 	}
 
 };
